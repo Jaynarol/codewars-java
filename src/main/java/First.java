5d981925e2049a3abb487d3dab1bc17b05915175
Jaynarol <jaynarol@gmail.com>
2016-11-04 14:16:45 +0700

    first commit

---
+++ src/main/java/First.java
@@ -0,0 +1,6 @@
+public class First {
+
+    public static int Multiple(int a, int b){
+        return a * b;
+    }
+}
---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.assertEquals;
+
+public class FirstTest {
+
+    @org.junit.Test
+    public void Multiply2And3_ShouldBeReturn6() {
+        assertEquals(6, First.Multiple(2, 3));
+    }
+
+}
